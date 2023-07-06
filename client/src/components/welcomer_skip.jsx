9658f718db0b35e2dd5ef7bb37e5a1fbdfda1552
phasenull <55253035+phasenull@users.noreply.github.com>
2023-07-06 14:20:54 +0300

    fixed button

--- client/src/components/welcomer_skip.jsx
+++ client/src/components/welcomer_skip.jsx
@@ -1,14 +1,15 @@
 function WelcomerSkip(prop) {
 	return (
-		<button id="welcomer" className="overflow-clip group text-2xl select-none bg-secondary py-10 px-20 text-primary font-bold rounded-full text-center transition-all hover:scale-105
-		hover:bg-gradient-to-r hover:from-[#57cc99] to-blue-500">
-			{/* <img alt="rocket" className="h-20 hidden group-hover:inline-block" src="https://tr.rbxcdn.com/14b2a81ff16cea41af8dd289cd28635c/352/352/Avatar/Png" /> */}
-			<div className="z-5 transition-transform duration-0 out group-hover:duration-500 group-hover:-translate-y-32 -translate-x-32 group-hover:-translate-x-5">
-				<img className="z-5 absolute right-1/2 h-40 translate-y-[70%] hidden group-hover:block rotate-45 transition ease-in-out" src="https://static.vecteezy.com/system/resources/previews/013/743/608/original/cartoon-rocket-launch-png.png" alt="" />
+		<button id="welcomer" className="overflow-hidden group text-2xl select-none bg-secondary py-10 px-20 text-primary font-bold rounded-full text-center transition-all duration-500 hover:scale-105 hover:bg-secondary-darkest">
+			<div className="absolute w-full hidden group-hover:block -translate-y-14 translate-x-40">
+				<img className="z-1 w-2/3 object-cover opacity-0 transition duration-500 group-hover:opacity-100" src="https://www.pinclipart.com/picdir/big/561-5617080_galaxy-space-cosmic-cosmos-nebula-universe-stars-outer.png" alt="" />
 			</div>
-			<p className="z-1">
+			<div className="z-1 transition-transform duration-0 out group-hover:duration-1000 group-hover:-translate-y-4 -translate-x-64 translate-y-32 group-hover:-translate-x-24">
+				<img className="z-1 absolute h-40 hidden group-hover:block rotate-45" src="https://static.vecteezy.com/system/resources/previews/013/743/608/original/cartoon-rocket-launch-png.png" alt="" />
+			</div>
+			<p className="z-[6] relative group-hover:text-white">
 				GET STARTED
-				<p className="text-secondary-dark animate-pulse group-hover:animate-bounce z-1"> NOW</p>
+				<p className="text-secondary-darkest animate-pulse group-hover:animate-bounce z-6 group-hover:text-secondary"> NOW</p>
 			</p>
 		</button>
 	);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,13 +7,14 @@ module.exports = {
 				primary: "#80ed99",
 				"primary-dark": "#57cc99",
 				secondary: "#38a3a5",
-				"secondary-dark": "#22577a",
+				"secondary-dark": "#35a3a3",
+				"secondary-darkest": "#22577a",
 			},
 			animation: {
-				shake: "shake 1s cubic-bezier(.36,.07,.19,.97) both infinite",
+				rocket_shake: "shake 1s cubic-bezier(.36,.07,.19,.97) both infinite",
 			},
 			keyframes: {
-				shake: {
+				rocket_shake: {
 					"20%, 90%": { transform: "translate(-1px, 0)" },
 					"20%, 80%": { transform: "translate(2px, 0)" },
 					"30%, 50%, 70%": { transform: "translate(-4px, 0)" },
