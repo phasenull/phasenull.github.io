dbbbb9bb795b268b932c271344c5d7b84474ea6f
phasenull <55253035+phasenull@users.noreply.github.com>
2025-08-03 14:44:03 +0300

    added layout.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,21 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { BrowserRouter, createBrowserRouter, Route, Routes } from "react-router"
 import IndexPage from "./pages"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import Layout from "@components/layout"
+import RecentActivitiesPage from "@pages/recent-activities"
+const client = new QueryClient()
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<IndexPage />} />
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={client}>
+			<BrowserRouter>
+				<Layout>
+					<Routes>
+						<Route index element={<IndexPage />} />
+						<Route path="recent-activities" element={<RecentActivitiesPage />} />
+					</Routes>
+				</Layout>
+			</BrowserRouter>
+		</QueryClientProvider>
 	</StrictMode>
 )
