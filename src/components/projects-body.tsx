2401c493d029518d29c6239bb5d6e7e5c3a925eb
phasenull <55253035+phasenull@users.noreply.github.com>
2025-08-03 14:47:21 +0300

    projects-body

--- src/components/projects-body.tsx
+++ src/components/projects-body.tsx
@@ -1,11 +1,20 @@
+import { useGetAllProjects } from "@common/hooks"
+
 export default function ProjectsBody() {
+	const {error, data, isLoading, isFetching } = useGetAllProjects()
+	if (isLoading || isFetching) return <a>loading state</a>
+	if (!data || error) return <a>something went wrong: {error?.message} </a>
 	return (
-		<div className="">
-			<h4 className="text-2xl lg:text-4xl font-bold font-sans text-center">projects i'm proud to display</h4>
-			{new Array(100).fill(true).map((e, i) => (
-				<a>
+		<div className="bg-white">
+			<h4 className="text-2xl lg:text-4xl font-bold font-sans text-center">
+				projects i'm proud to display
+			</h4>
+			{[...data.projects,...data.projects,...data.projects,...data.projects].map((project,index) => (
+				<a key={project.id}>
 					<br />
-					Random Project-{i}
+					Random Project-{index}
+					{project.title}
+					{project.description	}
 				</a>
 			))}
 		</div>
