cb8b20b9d7b064a13eba362324f032d2f13d1f72
phasenull <55253035+phasenull@users.noreply.github.com>
2025-09-27 17:54:27 +0300

    mobile stack hover fix

--- src/pages/main/components/projects-body-stack-container.tsx
+++ src/pages/main/components/projects-body-stack-container.tsx
@@ -14,7 +14,7 @@ export default function ProjectsBodyStackContainer(props: { stack: any }) {
 				alt={stack.key}
 				className="object-contain h-8 w-8 border-[1px] border-slate-200 rounded-xl overflow-clip"
 			/>
-			<span className="absolute lg:px-2 lg:py-2 bg-white border-1 self-center  top-10 z-20 max-h-120 w-max max-w-60 hidden group-active:inline-block lg:group-hover:inline-block">
+			<span className="absolute lg:px-2 lg:py-2 bg-white border-1 self-center sm:top-10 sm:bottom-auto bottom-10 z-20 max-h-120 w-max max-w-60 hidden group-active:inline-block lg:group-hover:inline-block">
 				<h6>
 					{stack.key}
 					<div className="bg-blue-400 inline rounded-xl px-2 ml-4 text-white">
